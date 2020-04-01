6fc7c831348da2a20a0c3905f1f6411195fad584
Ariel Yssou <arielyssou@gmail.com>
2020-03-31 22:45:16 -0300

    beta version of the control.sh script

--- control.sh
+++ control.sh
@@ -20,17 +20,48 @@ function ctrl_c() {
 	exit 0
 }
 
+function help() {
+	clear
+	cat << EOF
+
+				control.sh 
+
+Description:
+	control.sh: A script to organizer and automatize all site activities.
+
+Synopsys
+	control.sh [-h] [-n <post name>] [-s <string>] [-u] [-p <post name>] 
+
+Options
+	-h|--help		Shows help text
+	-n|--new	Makes a new post. It wll be only a sketch untill you publish it
+	-s|--search Search a given string in all files
+	-p|--publish Publishes a uncommited post
+	-u|--upload Uploads new files to www server. It also updates all files that where edited
+
+Details
+	For futher details plase refer to the source code, for it's (hopefully) well documented.
+
+Authors
+	Ariel Yssou arielyssou@gmail.com
+
+Bugs
+	No bugs are know up to this date, which probably means that I haven't properly debuged this so if you find any please contact me.
+EOF
+}
+
 function update_targets () {
 	# Updates all lines within a html file with
 	# the <!--TARGET--> flag INPLACE
 	file_name="$1"
+
+	local origin
 	origin="$(realpath "$2")"
 
 	for target in $(cat "$file_name" | grep TARGET | tr ' ' '\n' | grep -E 'href|src'); do
 		target="$(echo "$target" | sed 's/href=//' | sed 's/src=//' | tr -d '"' | tr -d '>')"
 
 		new_target="$( (cd $(dirname $file_name) && realpath --relative-to=$origin $target) )"
-		echo "$new_target"
 
 		sed -i "s|\"$target\"|\"$new_target\"|g" "$file_name"
 	done
@@ -69,14 +100,16 @@ function new_post() {
 	return 0
 }
 
-function publish_main() {
+function publish() {
 	# Publishes an uncommited post
 
-	origin="$uncommitted/$1"
+	local origin destiny
+
+	origin="./uncommitted/$1"
 	if [[ ! -d "$origin" ]]; then
-		echo "\e[31mPost could not be found. Current uncommited posts:\e[0m"
+		echo "\e[31mPost could not be found. Current uncomitted posts:\e[0m"
 		ls "$uncommitted"
-		return 1
+		return 0
 	fi
 
 	case $2 in
@@ -95,55 +128,68 @@ function publish_main() {
 	esac
 
 	destiny+="/$1"
+	if [[ -d "./blog/$destiny" ]]; then
+		echo -e "\e[33mPost was already posted, aborting.\e[0m"
+		return 0
+	else
+		mkdir "./blog/$destiny"
+
+	# Move index.html
+	tmp="$origin/.temp"
+	cp "$origin/index.html" "$tmp"
+	update_targets "$tmp" "./blog/$destiny"
+	mv "$tmp" "./blog/$destiny/index.html"
+
+	echo -e "\e[91m""$origin/sketch.js""\e[0m"
+	cp "$origin/sketch.js" "./blog/$destiny"
 
 	# Post to main
-	tmp="$origin/.temp"
-	TEMP_FILES[${#TEMP_FILES[@]}]="$tmp"
+	tmp="$origin/.temp" #Temporary file to write post html to be included
+											#in the main index.html file
+	TEMP_FILES[${#TEMP_FILES[@]}]="$tmp" #To be cleaned if ctrl+c
 
 	title="$(cat $origin/index.html | grep -A 1 blog_title | tail -n1 | tr -d '\t')"
+	t_stamp="$(date +%s)"
+	cp "index.html" "./backup/main_$t_stamp.html"
 
 	cp "$templates/main_post.html" "$tmp"
 	sed -i "s|TARGET_TAG|./blog/$destiny|g" "$tmp"
 	sed -i "s/TITLE_TAG/$title/g" "$tmp"
-
-	ident="$(cat "test.html" | grep MAIN_TOP | sed 's/[^\t]//g')"
+	
+	ident="$(cat "test_main.html" | grep CURRENT_COL_TAG | sed 's/[^\t]//g')"
 	while read -r line; do
-		s="$(cat "$tmp" | grep "$line" | head -n1)"
-		echo "$s"
-		sed -i "/MAIN_TOP/a INPUT_LINE" './test.html'
-		sed -i "s|INPUT_LINE|$ident$s|g" './test.html'
+		new_line="$(cat "$tmp" | grep "$line" | head -n1)"
+		sed -i "/CURRENT_COL_TAG/a INPUT_LINE" './test_main.html'
+		sed -i "s|INPUT_LINE|$ident$new_line|g" './test_main.html'
 	done <<< $(tac $tmp)
 	rm $tmp
+
+	if [[ -n $(cat './test_main.html' | grep 'CURRENT_COL_TAG' | grep '1') ]]; then
+		sed -i "s/COL1 CURRENT_COL_TAG/COL1/" './test_main.html'
+		sed -i "s/COL2/COL2 CURRENT_COL_TAG/" './test_main.html'
+	else
+		sed -i "s/COL2 CURRENT_COL_TAG/COL2/" './test_main.html'
+		sed -i "s/COL1/COL1 CURRENT_COL_TAG/" './test_main.html'
+	fi
 
 	# Post to blog
 	tmp="$origin/.temp"
 	TEMP_FILES[${#TEMP_FILES[@]}]="$tmp"
+
+	cp "index.html" "./backup/blog_$t_stamp.html"
 
 	cp "$templates/blog_post.html" "$tmp"
 	sed -i "s|TARGET_TAG|./$destiny|g" "$tmp"
 	sed -i "s/TITLE_TAG/$title/g" "$tmp"
 	sed -i "s|DATE_TAG|$(date +%c)|g" "$tmp"
 
-	ident="$(cat "test.html" | grep MAIN_TOP | sed 's/[^\t]//g')"
+	ident="$(cat "./blog/test_blog.html" | grep BLOG_TOP | sed 's/[^\t]//g')"
 	while read -r line; do
-		s="$(cat "$tmp" | grep "$line" | head -n1)"
-		echo "$s"
-		sed -i "/MAIN_TOP/a INPUT_LINE" './test.html'
-		sed -i "s|INPUT_LINE|$ident$s|g" './test.html'
+		new_line="$(cat "$tmp" | grep "$line" | head -n1)"
+		sed -i "/BLOG_TOP/a INPUT_LINE" './blog/test_blog.html'
+		sed -i "s|INPUT_LINE|$ident$new_line|g" './blog/test_blog.html'
 	done <<< $(tac $tmp)
 	rm $tmp
-
-	# Move index.html
-	tmp="$origin/.temp"
-	TEMP_FILES[${#TEMP_FILES[@]}]=$tmp
-	cp "$origin/index.html" "$tmp"
-	update_targets "$tmp" "./blog/$destiny"
-	#mv $tmp
-	#rm $tmp
-
-	# Move sketch.js
-	cp "$origin/sketch.js" "./blog/$destiny"
-	#mv $tmp
 
 	return 0
 }
@@ -185,8 +231,8 @@ case $1 in
 		search ${@:2}
 		exit $?
 		;;
-	-pm|--main)
-		publish_main ${@:2}
+	-p|--publish)
+		publish ${@:2}
 		exit $?
 		;;
 esac
