6e3f5525bfe64e7dece5c68e45d75f0865354e8b
Ariel <arielyssou@gmail.com>
2020-08-16 16:54:01 -0300

    finished drawings RNN

--- uncommitted/RNN/sketch.js
+++ uncommitted/RNN/sketch.js
@@ -1,10 +1,170 @@
+function arrowTip(p, arrowSize, tip = 'simple') {
+	if(tip == 'triangle') {
+		p.triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+	} else if(tip == 'simple') {
+		p.line(0, arrowSize / 2, arrowSize, 0);
+		p.line(0, -arrowSize / 2, arrowSize, 0);
+	} else if(tip == 'fancy') {
+		p.beginShape();
+		p.vertex(0, arrowSize / 2);
+		p.quadraticVertex(arrowSize / 4, 0, arrowSize, 0);
+		p.vertex(arrowSize, 0);
+		p.endShape();
+
+		p.beginShape();
+		p.vertex(0, -arrowSize / 2);
+		p.quadraticVertex(arrowSize / 4, 0, arrowSize, 0);
+		p.vertex(arrowSize, 0);
+		p.endShape();
+	}
+	else {
+		// No tip
+	}
+}
+
+function drawArrow(p, base, vec, clr = '#ffeabc', tip = 'simple') {
+	vec = vec.copy().sub(base).copy();
+  p.push();
+  p.stroke(clr);
+  p.strokeWeight(1.3);
+	p.strokeCap(p.ROUND);
+  p.fill(clr);
+  p.translate(base.x, base.y);
+  p.line(0, 0, vec.x, vec.y);
+  p.rotate(vec.heading());
+
+  let arrowSize = 7;
+	p.translate(vec.mag() - arrowSize, 0);
+	arrowTip(p, arrowSize, tip);
+
+  p.pop();
+}
+
+function pathArrow(p, coords, clr = '#ffeabc', tip = 'simple') {
+	let points = [];
+	for(var i = 0; i < coords.length; i += 2) {
+		points.push( p.createVector(coords[i], coords[i + 1]) );
+	}
+
+	p.push();
+	p.stroke(clr);
+	p.strokeWeight(3);
+	p.strokeCap(p.ROUND);
+	p.fill(clr);
+
+	for(var i = 0; i < points.length - 1; i += 1) {
+		p.push()
+
+		let base = points[i].copy();
+		let vec = points[i+1].copy().sub(base).copy();
+
+		p.translate(base.x, base.y);
+		p.line(
+			0, 0,
+			vec.x, vec.y
+		);
+		p.pop();
+	}
+
+	let end = points.length - 1;
+	base = points[end - 1].copy();
+	vec = points[end].copy().sub(base).copy();
+	p.translate(base.x, base.y);
+	p.line(
+		0, 0,
+		vec.x, vec.y
+	);
+	p.rotate(vec.heading());
+
+  let arrowSize = 7;
+	p.translate(vec.mag() - arrowSize, 0);
+	arrowTip(p, arrowSize, tip);
+
+	p.pop();
+}
+
+function curvedArrow(p, points, curvature = 20, clr = '#ffeabc', tip = 'fancy') {
+	// Draws a arrow throught the list of given points with curved corners
+	// INPUT:
+	// 	p: P5.JS
+	// 	points: p5.js.vector list
+	
+	let vertexes = [];
+	for(var i = 0; i < points.length; i += 1) {
+		if( (i - 1) >= 0 && (i + 1) < points.length ) { // top or head of arrow
+			// Elbow start
+			let delta = points[i].copy().sub(points[i-1])  
+			let start = points[i-1].copy().add(delta.sub(delta.copy().setMag(curvature)));
+			vertexes.push(
+				{
+					'point' : start,
+					'type' : 'vertex'
+				}
+			);
+			// Elbow control point
+			vertexes.push(
+				{
+					'point' : points[i],
+					'type' : 'anchor'
+				}
+			);
+			// Elbow end
+			vertexes.push(
+				{
+					'point' : points[i].copy().add(points[i + 1].copy().sub(points[i]).setMag(curvature)),
+					'type' : 'vertex'
+				}
+			);
+		} else {
+			vertexes.push(
+				{
+					'point' : points[i],
+					'type' : 'vertex'
+				}
+			);
+		}
+	}
+	p.push();
+	p.stroke(clr);
+	p.strokeWeight(1.3);
+	p.strokeCap(p.ROUND);
+	p.noFill();
+
+	p.beginShape()
+	for(var i = 0; i < vertexes.length; i += 1) {
+		if( vertexes[i].type == 'vertex' ) {
+			p.vertex(vertexes[i].point.x, vertexes[i].point.y);
+		} else if (vertexes[i].type == 'anchor') {
+			p.quadraticVertex(
+				vertexes[i].point.x, vertexes[i].point.y,
+				vertexes[i+1].point.x, vertexes[i+1].point.y
+			);
+		}
+	}
+	p.endShape()
+	p.fill(clr);
+
+	let end = points.length - 1;
+	base = points[end - 1].copy();
+	vec = points[end].copy().sub(base).copy();
+	p.translate(base.x, base.y);
+	p.rotate(vec.heading());
+
+  let arrowSize = 7;
+	p.translate(vec.mag() - arrowSize, 0);
+	arrowTip(p, arrowSize, tip);
+
+	p.pop();
+}
+
 // ############################################################
-function Node(p, x, y, radius, facecolor = null, edgecolor = "#ffffff") {
+function Node(p, x, y, radius, facecolor = null, edgecolor = "#ffeabc", annot = '') {
 	// A constructor function for regular neurons representaed by circles
 	this.pos = p.createVector(x, y);
 	this.radius = radius;
 	this.facecolor = facecolor;
 	this.edgecolor = edgecolor;
+	this.annot = annot;
 
 	this.show = function() {
 		p.push();
@@ -15,10 +175,15 @@ function Node(p, x, y, radius, facecolor = null, edgecolor = "#ffffff") {
 		}
 
 		p.stroke(this.edgecolor);
+		p.strokeWeight(1.5)
 		p.ellipse(this.pos.x, this.pos.y, 2 * this.radius, 2 * this.radius);
+		if(this.annot != '') {
+			p.textSize(this.radius * -1.7);
+			p.textAlign(p.CENTER, p.CENTER)
+			p.text(this.annot, this.pos.x, this.pos.y);
+		}
 		p.pop();
 	};
-
 }
 
 function Layer(p, x, y, width, height, total_nodes = 0) {
@@ -34,6 +199,7 @@ function Layer(p, x, y, width, height, total_nodes = 0) {
 			this.nodes[i].show();
 		}
 	};
+
 }
 
 function Dense(p, x, y, width, height, total_nodes = 0) {
@@ -61,8 +227,9 @@ function Dense(p, x, y, width, height, total_nodes = 0) {
 	}
 }
 
-function Network(p) {
+function Network(p, connection_style = '') {
 	this.layers = [];
+	this.connection_style = connection_style;
 
 	this.add = function(layer) {
 		this.layers.push(layer); // Add check
@@ -71,16 +238,17 @@ function Network(p) {
 	this.draw_connections = function() {
 		p.push();
 		p.strokeWeight(0.7)
-		p.stroke('#F')
+		p.stroke('#ffeabc')
 		for(var layer = 0; layer < this.layers.length - 1; layer += 1) {
 			for(var pos1 = 0; pos1 < this.layers[layer].output_anchors.length; pos1 += 1) {
 				for(var pos2 = 0; pos2 < this.layers[layer + 1].input_anchors.length; pos2 += 1) {
-					p.line(
-						this.layers[layer].output_anchors[pos1].x,
-						this.layers[layer].output_anchors[pos1].y,
-						this.layers[layer + 1].input_anchors[pos2].x,
-						this.layers[layer + 1].input_anchors[pos2].y,
-						);
+					drawArrow(
+						p,
+						this.layers[layer].output_anchors[pos1],
+						this.layers[layer + 1].input_anchors[pos2],
+						'#ffeabc',
+						this.connection_style
+					);
 				}
 			}
 		}
@@ -97,7 +265,7 @@ function Network(p) {
 
 var s = function( p ) { // p could be any variable name
 	var nn = new Network(p);
-	nn.add( new Dense(p, 50, 10, 40, 250, 3) );
+	nn.add( new Dense(p, 50, 10, 40, 250, 3, 'input layer') );
 	nn.add( new Dense(p, 150, 10, 40, 250, 5) );
 	nn.add( new Dense(p, 250, 10, 40, 250, 5) );
 	nn.add( new Dense(p, 350, 10, 40, 250, 1) );
@@ -109,8 +277,19 @@ var s = function( p ) { // p could be any variable name
   };
 
   p.draw = function() {
-    p.background(0);
 		nn.show();
+
+		//Texts
+		p.push();
+		p.stroke('#ffeabc');
+		p.fill('#ffeabc');
+		p.textSize(12);
+		p.textAlign(p.CENTER, p.CENTER)
+		p.text('input layer', 50, 270);
+		p.text('hidden layers', 200, 270);
+		p.text('output layer',  350, 270);
+		p.pop();
+
 		p.noLoop()
   };
 
@@ -168,12 +347,10 @@ function BlobsCloud(p, x, y, clr = '#ffffff') {
 			let y = this.blobs[i].pos.y;
 			p.fill(255);
 			p.noStroke();
-			console.log(this.blobs[i].a)
 			p.ellipse(x, y, this.blobs[i].a, this.blobs[i].b);
 		};
 		p.pop();
 	}
-
 }
 
 function BezierCloud(p, x, y, clr = '#ffffff') {
@@ -202,9 +379,6 @@ function BezierCloud(p, x, y, clr = '#ffffff') {
 		aux1Radius.push(p.random(anchorRadius[i], anchorRadius[i] + 20))
 		aux2Radius.push(p.random(anchorRadius[i], anchorRadius[i] + 20))
 	}
-	console.log(anchorAngles)
-	console.log(aux1Angles)
-	console.log(aux2Angles)
 
 	this.shape = function () {
 		p.beginShape();
@@ -263,8 +437,6 @@ function CartoonCloud(p, x, y, clr = '#ffffff') {
 		auxRadius.push(p.random(anchorRadius[i] + 10, anchorRadius[i] + 20))
 	}
 
-	console.log(auxAngles)
-	console.log(anchorAngles)
 
 	this.shape = function () {
 		p.beginShape();
@@ -292,22 +464,210 @@ function CartoonCloud(p, x, y, clr = '#ffffff') {
 	};
 }
 
-
 // ############################################################
 // Sketch Two
+function Cell(p, x, y, width, height, radius, facecolor = null, edgecolor = "#ffeabc", annot = 'A') {
+	// x, y = center positions
+	Node.call(this, p, x, y, radius, facecolor, edgecolor); //Creates pos with (x, y)
+	this.width = width;
+	this.height = height;
+	this.annot = annot;
+
+	this.anchors = {
+		'top' : p.createVector(x, y - (height / 2)),
+		'left' : p.createVector(x - (width / 2), y ),
+		'right' : p.createVector(x + (width / 2), y),
+		'bottom' : p.createVector(x, y + (height / 2)),
+	};
+
+	this.show = function() {
+		p.push();
+
+		if(this.facecolor) {
+			p.fill(this.facecolor);
+		} else {
+			p.noFill();
+		}
+		p.stroke(edgecolor);
+		p.strokeWeight(1.5);
+
+		p.rect(
+			this.pos.x - (this.width / 2),
+			this.pos.y - (this.height / 2),
+			this.width,
+			this.height,
+			this.radius
+		);
+
+		if(this.annot != '') {
+			p.strokeWeight(1);
+			p.fill('#ffeabc')
+			p.textSize(this.radius * -1.7);
+			p.textAlign(p.CENTER, p.CENTER)
+			p.text(this.annot, this.pos.x, this.pos.y);
+		}
+
+		p.pop();
+	};
+}
+
+function recurrentLayer(p, x, y, width, height, index = '', arrow_style = 'fancy', folded = false) {
+	Layer.call(this, p, x, y, width, height, 1); // 1 is the number of nodes, stored in 'total_nodes'
+	this.input_anchors = [];
+	this.output_anchors = [];
+
+	// RNN have a single node, so there's no need for a loop
+	if(folded) {
+		//If fold there must be space for the recurring input arrow later on
+		//so we just use half the width for the layer
+		this.cell = new Cell(
+			p,
+			x + width / 2,
+			y + height / 2,
+			0.9 * (width / 2),
+			0.8 * (width / 2),
+			5
+		);
+	} else {
+		this.cell = new Cell(
+			p,
+			x + width / 2,
+			y + height / 2,
+			0.9 * width,
+			0.8 * width,
+			5
+		);
+		this.input_anchors.push(this.cell.anchors.left);
+		this.output_anchors.push(this.cell.anchors.right);
+	}
+
+	this.show = function() {
+		p.push();
+		// Draw arrows first so figures will be on top of then
+		let radius = 20
+		// Cell input
+		inputNode = new Node(
+			p,
+			x + (this.width / 2),
+			y + this.height - radius - 3,
+			radius,
+			facecolor = null,
+			'#ffeabc',
+			annot = 'x_'.concat(index)
+		)
+
+		drawArrow(
+			p,
+			p.createVector(x + (this.width / 2), y + this.height - 2 * radius - 3),
+			this.cell.anchors.bottom,
+			'#ffeabc',
+			arrow_style
+		);
+		inputNode.show();
+
+		// Cell Output
+		outputNode = new Node(
+			p,
+			x + (this.width / 2),
+			radius + 3,
+			radius,
+			facecolor = null,
+			'#ffeabc',
+			annot = 'h_'.concat(index)
+		)
+		drawArrow(
+			p,
+			this.cell.anchors.top,
+			p.createVector(this.pos.x + this.width / 2, 2 * radius + 3),
+			'#ffeabc',
+			arrow_style
+		);
+		outputNode.show();
+
+		if(folded){
+			// Recurring input
+			curvedArrow(
+				p,
+				[
+					this.cell.anchors.right,
+					p.createVector(
+						this.pos.x + this.width,
+						this.cell.anchors.right.y
+					),
+					p.createVector(
+						this.pos.x + this.width,
+						this.cell.anchors.right.y - 40
+					),
+					p.createVector(
+						this.pos.x,
+						this.cell.anchors.left.y - 40
+					),
+					p.createVector(
+						this.pos.x,
+						this.cell.anchors.left.y,
+					),
+					this.cell.anchors.left
+				],
+				curvature = 20
+			)
+		} else {
+			// pass
+		}
+
+		this.cell.show();
+		p.pop()
+	}
+}
+
+/*
+function empty_Layer(p, x, y, width, height) {
+	Layer.call(this, p, x, y, width, height, 1); // 1 is the number of nodes, stored in 'total_nodes'
+	this.input_anchors = [];
+	this.output_anchors = [];
+	this.show = function () {
+		p.push();
+		p.strokeWeight(1);
+		p.fill('#ffeabc')
+		p.textSize(2);
+		p.textAlign(p.CENTER, p.CENTER)
+		p.text('...', this.pos.x, this.pos.y + this.height);	
+		p.pop()
+	}
+}
+*/
+
 var t = function( p ) { 
-	//var cloud = new BlobsCloud(p, 100, 100);
-	var cloud = new CartoonCloud(p, 100, 100);
+	var RNN = new Network(p, connection_style = 'fancy');
+	RNN.add( new recurrentLayer(p, 50, 10, 80, 250, index = 't', 'fancy',  folded = true));
+	RNN.add( new recurrentLayer(p, 200, 10, 40, 250, index = '0', 'fancy'));
+	RNN.add( new recurrentLayer(p, 300, 10, 40, 250, index = '1', 'fancy'));
+	//RNN.add( new empty_Layer(p, 350, 10, 50, 250));
+	RNN.add( new recurrentLayer(p, 450, 10, 40, 250, index = 't', 'fancy'));
 
   p.setup = function() {
 		var myWidth = document.getElementById("c2").offsetWidth;
-		/*var myHeight = document.getElementById("c1").offsetHeight;*/
-
-    p.createCanvas(myWidth, 200);
+    p.createCanvas(600, 400);
   };
 
   p.draw = function() {
-		cloud.draw();
+		RNN.show();
+		p.push()
+		p.stroke('#ffeabc')
+		p.strokeWeight(2)
+		p.line(
+			150, RNN.layers[0].height/2 + 5,
+			165, RNN.layers[0].height/2 + 5  
+		)
+		p.line(
+			150, RNN.layers[0].height/2 + 10,
+			165, RNN.layers[0].height/2 + 10
+		)
+		p.strokeWeight(1);
+		p.fill('#ffeabc')
+		p.textSize(20);
+		p.textAlign(p.CENTER, p.CENTER)
+		p.text('...', 390, 150);
+		p.pop()
 		p.noLoop();
   };
 
@@ -318,3 +678,32 @@ var t = function( p ) {
 	};
 };
 var myp52 = new p5(t, 'c2');
+
+
+
+
+var t = function( p ) { 
+	//var cloud = new BlobsCloud(p, 100, 100);
+	var cloud = new CartoonCloud(p, 100, 100);
+
+  p.setup = function() {
+		var myWidth = document.getElementById("c3").offsetWidth;
+		/*var myHeight = document.getElementById("c3").offsetHeight;*/
+
+    p.createCanvas(myWidth, 200);
+  };
+
+  p.draw = function() {
+		cloud.draw();
+		p.noLoop();
+  };
+
+	p.windowResized = function() {
+		mwidth = document.getElementById("c3").offsetWidth;
+		/*mheight = document.getElementById("c3").offsetHeight;*/
+		p.resizeCanvas(mwidth, p.height);
+	};
+};
+var myp52 = new p5(t, 'c3');
+
+
