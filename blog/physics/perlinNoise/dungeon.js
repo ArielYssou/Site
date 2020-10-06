64a8eec280cf22c6013aeb092fe9f23ec42996eb
Ariel <arielyssou@gmail.com>
2020-10-05 22:24:54 -0300

    begin implement the hoshen-kopelman algorithm

--- blog/physics/perlinNoise/dungeon.js
+++ blog/physics/perlinNoise/dungeon.js
@@ -1,8 +1,8 @@
-function Cluster() {
+function Cluster(anchor) {
 	this.sites = [];
 	this.angles = [];
 	this.convexHull = [];
-	this.anchor = 0;
+	this.anchor = anchor;
 
 	this.detectHull = function() {
 		// Grahan algorithm
@@ -24,11 +24,22 @@ function Cluster() {
 		this.sites = dsu(this.sites, this.angles);
 		this.angles.sort().reverse();
 		
+		this.ccw = function(p1, p2, p3) {
+			// Counter Clock Wise detector.
+			// Returns ccw > 0 if counter clockwise, 0 if colinear and < 0 if clock wise
+			return ((p2.x - p1.x) * ( p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x))
+		}
+
 		var stack = [];
 		for(var i = 0; i < this.points.length; i += 1) {
 			// pop the last point from the stack if we turn clockwise to reach this point
-			while stack.length > 1	&&
-				ccw(stack[stack.length - 2], stack[stack.length - 1], this.points[i]) <= 0 {
+			if(i > 0) {
+				if(this.angles[i] > this.angles[i - 1])
+					stack.pop();
+			}
+
+			var l = stack.length; // Improve readability
+			while stack.length > 1	&& this.ccw(l - 2], stack[l - 1], this.points[i]) <= 0 {
 				stack.pop();
 			}
 			stack.push(this.points[i]);
@@ -51,16 +62,34 @@ function perlinMaze(p, scl, thresh = 0.4; clr, backgroud) {
 	this.rows = p.floor(p.height / scl);
 	
 	this.sites = new Array(this.cols * this.rows);
-	for(var i = 0; i < this.cols * rows; i++) {
-		this.sites[i] = false;
+	this.labels = new Array(this.cols * this.rows);
+	this.up = new Array(this.cols * this.rows);
+	this.right = new Array(this.cols * this.rows);
+
+	for(var y = 0; y < this.rows; y++) {
+		for(var x = 0; x < this.cols; x++){
+			var index = x + y * this.cols;
+			this.sites[index] = false;
+			this.labels[inde] = index;
+
+			if(y != 0)
+				this.up[index] = index - this.cols
+			else
+				this.up[index] = null;
+
+			if(x > 0)
+				this.left[index] = index - 1;
+			else
+				this.left[index] = null;
+		}
 	}
 
 	this.update = function() {
 		this.yoff = this.zoff;
-    for(var y = 0; y < rows; y++) {
+    for(var y = 0; y < this.rows; y++) {
 			this.xoff = this.zoff;
-      for(var x = 0; x < cols; x++){
-        var index = x + y * cols
+      for(var x = 0; x < this.cols; x++){
+        var index = x + y * this.cols
 				this.sites[index] = p.noise(this.xoff, this.yoff, this.zoff) > this.thresh;
 
         this.xoff += this.xSpeed;
@@ -83,8 +112,63 @@ function perlinMaze(p, scl, thresh = 0.4; clr, backgroud) {
   }
 
 	this.detect_clusters = function {
+		// Hoshen - Kopelman algorithm
+		// See:
+		// [1] - Very confusing but correct:
+		// https://en.wikipedia.org/wiki/Hoshen%E2%80%93Kopelman_algorithm
+		// [2] - More explanatory. Complements [1] well:
+		// https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiN4_--4Z7sAhU7JbkGHcYBCP0QFjAFegQIBhAC&url=https%3A%2F%2Fwebhome.weizmann.ac.il%2Fhome%2Ffeamit%2Fnodalweek%2Fc_joas_nodalweek.pdf&usg=AOvVaw3AwGXXqVZN-mBWmjoB3YTl 
+
+		let above;
+		let left;
+
+		find = function(x) {
+			let y = x;
+			while(this.labels[y] != y)
+				y = this.labels[y];
+			while(this.labels[x] != x) {
+				let z = this.labels[x];
+				this.labels[x] = y;
+				x = z;
+			}
+			return y;
+		}
+
+		union = function(x, y) {
+			this.labels[find(x)] = find(y);
+		}
+
+		const union(x, y) => this.labels
+
+		global_label = 0
     for(var y = 0; y < rows; y++) {
-      for(var x = 0; x < cols; x++){
+      for(var x = 0; x < cols; x++) {
+				var index = x + y * cols;
+				if(this.sites[index]) {
+					if(x == 0)
+						above = 0;
+					else
+						above = this.labels[this.up[index]];
+					if(y == 0)
+						left = 0;
+					else
+						left = this.labels[this.left[index]];
+
+					if((left == 0) && (above == 0)){
+						largest_label += 1;
+						this.labels[index] = largest_label;
+						this.clusters = new Cluster(p.createVector(x * scl, y * scl));
+					} else if ((left != 0) && (above == 0) {
+						this.labels[index] = find(left);
+					} else if (left == 0) && (above != 0) {
+						this.labels[index] = find(above);
+					} else {
+						union(left, above);
+						this.labels[index] = find[left]
+					}
+				}	
+			}
+		}
 	}
 }
 
