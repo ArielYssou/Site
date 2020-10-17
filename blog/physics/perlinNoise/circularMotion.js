46b4bd74bdb142371dfc399b09a51139864136b5
Ariel <arielyssou@gmail.com>
2020-10-17 20:21:14 -0300

    modifications to perlin

---
+++ blog/physics/perlinNoise/circularMotion.js
@@ -0,0 +1,85 @@
+var background = '#363530';
+
+function Ball(p, pos, radialSpeed = 0, angularSpeed = 0.03, mass = 10) {
+	this.pos = pos;
+	this.angularSpeed = angularSpeed;
+	this.radialSpeed = radialSpeed;
+	this.mass = mass;
+
+	this.radius = this.pos.mag();
+	this.clr = p.color('#F4EBBE');
+	this.lastPos = this.pos.copy();
+
+	this.radialXoff = p.random();
+	this.radialYoff = p.random();
+	this.radialXSpeed = 0.02;
+	this.radialYSpeed  = 0.02;
+
+	this.update = function(angularAcc = 0) {
+		let rad_random = p.noise(this.radialXoff, this.radialYoff)  
+		this.radialSpeed = this.radius + 80 * (rad_random - 0.5);
+  
+		this.lastPos = this.pos.copy()
+		this.pos.rotate(this.angularSpeed);
+		this.pos.setMag(this.radialSpeed);
+
+		this.radialXoff += this.radialXSpeed;
+		this.radialYoff += this.radialYSpeed;
+	}
+
+	this.show = function() {
+		p.push()
+		p.strokeWeight(2)
+		p.fill(this.clr)
+		p.stroke(this.clr)
+
+		p.line(
+			this.lastPos.x,
+			this.lastPos.y,
+			this.pos.x,
+			this.pos.y
+		);
+		p.pop()
+	}
+}
+
+var s = function( p ) {
+	var balls = new Array();
+  p.setup = function() {
+    var myWidth = document.getElementById("circularMotion").offsetWidth;
+    p.createCanvas(600, 400);
+		for(var i = 0; i < 100; i += 1) {
+			balls.push(
+				new Ball(
+					p,
+					p.constructor.Vector.random2D().mult(
+						p.map(p.random(),0,1, 120, 180)
+					),
+					0,
+					p.map(p.random(), 0, 1, 0.02, 0.04)
+				)
+			)
+		}
+  }
+
+  p.draw = function() {
+		if(p.frameCount == 1) {
+			p.background(p.color(54, 53, 48));
+		}
+
+		p.background(p.color(54, 53, 48, 45));
+		p.translate(p.width / 2, p.height / 2);
+		for(ball of balls) {
+			ball.show();
+			ball.update(p.random());
+		}
+  }
+
+  p.windowResized = function() {
+    mwidth = document.getElementById("circularMotion").offsetWidth;
+
+    p.resizeCanvas(mwidth, p.height);
+  };
+};
+
+var myp51 = new p5(s, 'circularMotion');
--- blog/physics/perlinNoise/waterBall.js
+++ blog/physics/perlinNoise/waterBall.js
@@ -1,16 +1,18 @@
-Site = function(x, y, value) {
+Site = function(x, y, value, active = false, health = 0) {
 	this.x = x;
 	this.y = y;
 	this.value = value;
+	this.active = active;
+	this.health = health;
 }
 
-waterBall = function(p, scl, mouseRadius = 5, thresh = 0.5, ceil = 10, clr = 255, background = 0) {
+waterBall = function(p, scl, mouseRadius = 15) {
 	this.scl = scl;
 	this.mouseRadius = mouseRadius; // raidius of the mouse  
-	this.thresh = thresh;
-	this.ceil = ceil;
-	this.clr = clr;
-	this.background = background;
+	this.thresh = 0.4;
+	this.clr = p.color('#CACC90');
+	this.background = p.color('#363530');
+	this.noiseMax = 0.7; // Noise amplitude
 
 	this.cols = p.floor(p.width / this.scl);
 	this.rows = p.floor(p.height / this.scl);
@@ -22,40 +24,58 @@ waterBall = function(p, scl, mouseRadius = 5, thresh = 0.5, ceil = 10, clr = 255
 	this.activeNoiseSites = new Array();
 
 	// Mouse hover masks the function values
-	this.mask = new Array(this.mouseRadius, this.mouseRadius);
+	this.mouseDiam = 1 + (2 * this.mouseRadius);
+	this.mouseMiddle = this.mouseRadius + 1;
+	this.mask = new Array();
 
 	// Sites masked by the mouse that need to be updated back
-	this.needUpdate = new Array();
-	this.recoverSpeed = 1 / 10; // After hidden, cells will take 10 frames to show completely
+	this.needUpdate = {};
+	this.recoverSpeed = 1 / 17; // After hidden, cells will take 10 frames to show completely
 
 	// Noise space speeds and offsets
 	this.xoff = this.yoff = this.zoff = 0;
-	this.xSpeed = this.ySpeed = 0.3; // noise space speed
-	this.zSpeed = 0.01;
-	this.noiseMax = 0.9 * this.ceil; // Noise amplitude
+	this.xSpeed = 0.2;
+	this.ySpeed = 0.2; // noise space speed
+	this.zSpeed = 0.2;
 
 	// Shape function and radius (since its a paraboloid)
-	this.radius = 20 * this.scl;
+	this.radius = 30 * this.scl;
 	this.shape_function = function(x, y) {
 		let xs = (x - (p.width/2) - this.radius) * (x - (p.width/2) + this.radius);
 		let ys = (y - (p.height/2) - this.radius) * (y - (p.height/2) + this.radius);
-		return this.ceil - xs - ys
+		return 1 - xs - ys
 	}
 
 	this.init = function() {
 		// Initializes the function map, noise and map arrays
+		var normalization = -Infinity;
+    for(var y = 0; y < this.rows; y++) {
+      for(var x = 0; x < this.cols; x++){
+				var index = x + y * this.cols;
+				var value = this.shape_function(this.scl * x, this.scl * y);
+				this.sites[index] =  new Site(x, y, value, false);
+				normalization = value > normalization ? value  : normalization
+			}
+		}
+		for(site of this.sites) {
+			site.value /= normalization;
+		}
+
+		// Apply noise and set active sites
 		this.yoff = this.zoff;
     for(var y = 0; y < this.rows; y++) {
 			this.xoff = this.zoff;
       for(var x = 0; x < this.cols; x++){
 				var index = x + y * this.cols;
 				var noise = this.noiseMax * p.noise(this.xoff, this.yoff, this.zoff);  
-				var value = this.shape_function(this.scl * x, this.scl * y);
+				if((this.sites[index].value + noise) / (1 + this.noiseMax) > this.thresh) {
+					this.sites[index].health = 1;
+					this.sites[index].active = true;
+				}
   
-				if(value >= -this.noiseMax) { // Can be active
-					this.sites[index] = value + noise > 0 ? 1 : 0;
-					if(value <= this.noiseMax) {
-						this.activeNoiseSites.push(new Site(x, y, value));
+				if(this.sites[index].value >= -this.noiseMax) { // Can be active
+					if(this.sites[index].value <= this.noiseMax) {
+						this.activeNoiseSites.push(this.sites[index]);
 					}
 				}
 				this.xoff += this.xSpeed;
@@ -64,11 +84,15 @@ waterBall = function(p, scl, mouseRadius = 5, thresh = 0.5, ceil = 10, clr = 255
 		}
 		this.zoff += this.zSpeed;
 
-		for(var dy = 0; dy <= 2 * this.mouseRadius; dy += 1) {
-			for(var dx = 0; dx <= 2 * this.mouseRadius; dx += 1) {
-				if(p.dist(this.mouseRadius / 2, this.mouseRadius / 2, dx, dy) < this.mouseRadius) {
-					value = ((dx * dx) + (dy * dy)) / (2 * this.mouseRadius ** 2) 
-					this.mask.push(new Site(dx, dy, value))
+		// Make mouse maks values
+		for(var dy = -this.mouseRadius; dy <= this.mouseRadius; dy += 1) {
+			for(var dx = -this.mouseRadius; dx <=  this.mouseRadius; dx += 1) {
+				if(p.dist(0, 0, dx, dy) < this.mouseRadius) {
+					this.mask.push(
+						p.min(((p.dist(0, 0, dx, dy) / this.mouseRadius) - 0.7), 1)
+					)
+				} else {
+					this.mask.push(1)
 				}
 			}
 		}
@@ -76,75 +100,102 @@ waterBall = function(p, scl, mouseRadius = 5, thresh = 0.5, ceil = 10, clr = 255
 	this.init();
 
 	this.updateNoise = function() {
-		for(const site of this.sites) {
+		for(var i = 0; i < this.activeNoiseSites.length; i += 1) {
+			var site = this.activeNoiseSites[i];
+			var index = site.x + site.y * this.cols;
+
 			this.xoff = this.zoff + this.xSpeed * site.x;
 			this.yoff = this.zoff + this.ySpeed * site.y;
-			site.value = this.noiseMax * p.noise(this.xoff, this.yoff, this.zoff)
+			noise = this.noiseMax * p.noise(this.xoff, this.yoff, this.zoff)
+			if((site.value + noise) / (1 + this.noiseMax) > this.thresh) {
+				if(! this.sites[index].active) {
+					this.sites[index].health = 0
+					this.sites[index].active = true;
+					this.needUpdate[index] = this.sites[index]; 
+				}
+			} else {
+				if(this.sites[index].active) {
+					this.sites[index].active = false;
+					this.needUpdate[index] = this.sites[index]; 
+				}
+			}
 		}
 		this.zoff += this.zSpeed;
 	}
 
 	this.applyMouse = function(mx, my) {
-		var posx = = p.floor((mx / this.scl);
-		var posy = = p.floor((my / this.scl);
-		var index = posx + posy * this.cols;
+		// Implement minmun and maximum x and y values to ignore part of the screen
+		var posx = p.floor((mx / this.scl));
+		var posy = p.floor((my / this.scl));
+		let maskIndex = 0;
 
-		let maskY = 0
-		for(var dy = -p.floor(this.mouseRadius/2); dy <= -p.floor(this.mouseRadius/2); dy += 1) {
-			let maskX = 0
-			for(var dx = -p.floor(this.mouseRadius/2); dx <= -p.floor(this.mouseRadius/2); dx += 1) {
-				let maskIndex = maskX + maskY * this.mouseRadius;
-				if(this.sites[index] == 1)	 {
-					this.needUpdate.push(
-						{
-							'health': this.mask[maskIndex][i],
-							'x' : posx + dx,
-							'y' : posy + dy
-						}
+		for(var dy = - this.mouseRadius; dy <= this.mouseRadius; dy += 1) {
+			if((posy + dy < 0) || (posy + dy > this.rows)) {continue;}
+			for(var dx = - this.mouseRadius; dx <= this.mouseRadius; dx += 1) {
+				if(posx + dx < 0 || posx + dx > this.cols) {continue;}
+				maskIndex = (dx + this.mouseRadius)
+					+ (dy + this.mouseRadius) * (this.mouseDiam);
+				let absoluteIndex = (posx + dx) + this.cols * (posy + dy);
+
+				if(absoluteIndex >= this.sites.length)
+					continue;
+				if(this.sites[absoluteIndex].active) {
+					this.sites[absoluteIndex].health = p.min(
+						this.mask[maskIndex],
+						this.sites[absoluteIndex].health
+					)
+					this.needUpdate[absoluteIndex] = this.sites[absoluteIndex]
+				} else if (this.sites[absoluteIndex].health > 0) {
+					this.sites[absoluteIndex].health = p.min(
+						this.mask[maskIndex],
+						this.sites[absoluteIndex].health
 					)
 				}
-				maskX += 1;
 			}
-			maskY += 1;
 		}
 	}
 
 	this.show = function() {
-    for(var y = 0; y < this.rows; y++) {
-      for(var x = 0; x < this.cols; x++){
-				let index = x + y * this.cols
-				let c = this.clr
-				if(this.sites[index] < 0) {
-					//c = this.background
-					continue;
-				}
-				p.fill(c);
-				p.noStroke();
-				p.circle(x * this.scl, y * this.scl, this.scl);
-      }
+		for(const site of this.sites) {
+			let c = this.clr
+			if(! site.active) {
+				//c = this.background;
+				continue;
+			}
+			p.fill(c);
+			p.noStroke();
+			p.circle(site.x * this.scl, site.y * this.scl, this.scl);
     }
   }
 
 	this.updateShow = function() {
-		for(var i = 0; i < this.needUpdate.length; i++) {
+		for(const pos of Object.keys(this.needUpdate)) {
+			//console.log(pos)
 			p.fill(
 				p.lerpColor(
-					p.color(this.background, this.background, this.background),
-					p.color(this.clr,this.clr, this.clr),
-					this.needUpdate[i]['health']
+					this.background,
+					this.clr,
+					this.needUpdate[pos].health
 				)
 			)
 			p.noStroke()
 			p.circle(
-				this.needUpdate[i]['x'] * this.scl,
-				this.needUpdate[i]['y'] * this.scl,
+				this.needUpdate[pos].x * this.scl,
+				this.needUpdate[pos].y * this.scl,
 				this.scl
 			)
-			//console.log(this.needUpdate[i]['x'])
-			//console.log(this.needUpdate[i]['y'])
-			this.needUpdate[i]['health'] += this.recoverSpeed;
-			if(this.needUpdate[i]['health'] > 1) {
-				this.needUpdate.splice(i, 1);
+			if(this.needUpdate[pos].active) {
+				this.needUpdate[pos].health += this.recoverSpeed;
+				this.sites[pos].health += this.recoverSpeed;
+				if(this.needUpdate[pos].health >= 1) {
+					delete this.needUpdate.pos;
+				}
+			} else {
+				this.needUpdate[pos].health -= this.recoverSpeed;
+				this.sites[pos].health -= this.recoverSpeed;
+				if(this.needUpdate[pos].health <= 0) {
+					delete this.needUpdate.pos;
+				}
 			}
 		}
 	}
@@ -155,17 +206,23 @@ var s = function( p ) {
   p.setup = function() {
     var myWidth = document.getElementById("waterBall").offsetWidth;
     p.createCanvas(myWidth, 400);
-		ball = new waterBall(p, 6);
+		ball = new waterBall(p, 2);
   }
 
   p.draw = function() {
-		p.background(ball.background)
-		ball.show()
+		if(p.frameCount == 1) {
+			p.background(ball.background)
+			ball.show()
+			ball.show()
+			ball.show()
+			ball.show()
+			ball.show()
+		}
 		//ball.applyMouse(p.mouseX, p.mouseY)
-		//ball.updateNoise();
-		ball.applyMouse(p.width / 2, p.height / 2)
+		ball.updateNoise();
+		//ball.applyMouse(p.width / 2, p.height / 2)
 		ball.updateShow();
-		p.noLoop();
+		//p.noLoop();
   }
 
   p.windowResized = function() {
