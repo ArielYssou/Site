37afe5c9d188653200d50281502fd5303d0fc082
ArielYssou <arielyssou@gmail.com>
2021-04-01 20:30:16 -0300

    changes to tree script

--- uncommitted/randomForest/tree.js
+++ uncommitted/randomForest/tree.js
@@ -14,27 +14,6 @@ function Branch(p, length, angle, width, color, depth) {
 	this.main_branch = new Leaf();
 	this.left_branch = new Leaf();
 
-	// Branches are split in sub_branches for visual effect
-//	this.sub_branches = new Array();
-//	this.partitions = int(p.rand(5, 10));
-//
-//	var remaining_length = this.length;
-//	for(var i = 0; i < this.partitions; i += 1) {
-//		var sub_length = p.random(1, remaining_length);
-//		var delta_angle = p.random(p.QUARTER_PI, P.QUARTER_PI) * 0.001;
-//		this.sub_branches.push(
-//			new Branch(
-//				p,
-//				sub_length,
-//				this.angle + delta_angle,
-//				this.width,
-//				this.color,
-//				this.depth
-//			)
-//		)
-//		remaining_length -= sub_length;
-	//}
-
 	this.show = function() {
 		p.push()
 		p.rotate(this.angle);
@@ -44,15 +23,26 @@ function Branch(p, length, angle, width, color, depth) {
 		this.right_branch.show();
 		p.pop()
 
+		if(this.main_branch) {
+			p.push()
+			p.rotate(this.angle);
+			p.translate(this.length, 0);
+			this.main_branch.show();
+			p.pop()
+		}
+
 		p.push()
 		p.rotate(this.angle);
 		p.translate(this.length, 0);
 		this.left_branch.show();
 		p.pop()
 	}
+
+	this.get_lines = function(points, global_points) {
+	}
 }
 
-create_branch = function(p, length, angle, width, color, depth, max_depth) {
+create_branch = function(p, length, angle, width, color, depth, max_depth, is_main) {
 	// Recusive function to generate a tree
 	// Stop condition
 	if (depth >= max_depth) {
@@ -62,9 +52,29 @@ create_branch = function(p, length, angle, width, color, depth, max_depth) {
 	// current branch
 	var branch = new Branch(p, length, angle, width, color, depth)
 
+	// Main branch
+	//length_delta = p.random(0.94, 0.97);
+	if(is_main) {
+		length_delta = 0.9;
+		angle_delta = p.random(-p.QUARTER_PI, p.QUARTER_PI) * 0.01;
+		width_delta = 0.7;
+		branch.main_branch = create_branch (
+			p,
+			length * length_delta,
+			angle_delta,
+			width * width_delta,
+			color,
+			depth + 1,
+			max_depth,
+			true
+		)
+	} else {
+		branch.main_branch = null;
+	}
+
 	// Rigt branch
 	//length_delta = p.random(0.94, 0.97);
-	length_delta = p.random(0.6, 1.3);
+	length_delta = p.random(0.6, 1.05);
 	angle_delta = p.random(-p.QUARTER_PI, p.QUARTER_PI) * 0.5;
 	width_delta = p.random(0.6, 0.8);
 	branch.right_branch = create_branch (
@@ -74,12 +84,13 @@ create_branch = function(p, length, angle, width, color, depth, max_depth) {
 		width * width_delta,
 		color,
 		depth + 1,
-		max_depth
+		max_depth,
+		false
 	)
 
 	// left branch
 	//length_delta = p.random();
-	length_delta = p.random(0.1, 1.2);
+	length_delta = p.random(0.6, 1.05);
 	width_delta = p.random(0.1, 0.5);
 	angle_delta = p.random(-p.QUARTER_PI, p.QUARTER_PI) * 0.05;
 	branch.left_branch = create_branch (
@@ -89,11 +100,16 @@ create_branch = function(p, length, angle, width, color, depth, max_depth) {
 		width * width_delta,
 		color,
 		depth + 1,
-		max_depth
+		max_depth,
+		false
 	)
 
 	return branch
 }
+
+var main_lines;
+
+
 
 
 function Tree(p, pos_x, pos_y, root_length, root_width, root_clr) {
@@ -105,7 +121,7 @@ function Tree(p, pos_x, pos_y, root_length, root_width, root_clr) {
 	this.max_depth = p.random(4, 6)
 
 
-	this.tree = create_branch(p, root_length, 0, root_width, root_clr, 0, this.max_depth);
+	this.tree = create_branch(p, root_length, 0, root_width, root_clr, 0, this.max_depth, true);
 	// this.tree.color = p.color(100,0,0);
 	console.log(this.tree)
 
