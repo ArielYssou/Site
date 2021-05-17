4132d04195b481eab4802d05a772d00643607347
ArielYssou <arielyssou@gmail.com>
2021-05-16 21:40:16 -0300

    improvements to data visualization

--- uncommitted/randomForest/feature_space_split.js
+++ uncommitted/randomForest/feature_space_split.js
@@ -12,23 +12,41 @@ var svg = d3.select("#feature_space_split")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+// Create data
+var classes = [0, 1]
+var myColor = d3.scaleOrdinal().domain(classes)
+  .range(["#FF934F", "#577399"])
+
 //Read the data
 d3.csv("https://raw.githubusercontent.com/ArielYssou/Site/master/uncommitted/randomForest/example_data.csv", function(data) {
 
   // Add X axis
-  var x = d3.scaleLinear()
-    .domain([15, 10])
+  var x_axis = d3.scaleLinear()
+    .domain([-15, 10])
     .range([ 0, width]);
+
   svg.append("g")
+		.attr("class", "axisDarkTheme")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x_axis));
+
+	svg.append("text")
+			.attr("class", "legend__label")
+			.attr("text-anchor", "end")
+			.attr("x", width)
+			.attr("y", height - 6)
+			.text("x1");
+
 
   // Add Y axis
-  var y = d3.scaleLinear()
+  var y_axis = d3.scaleLinear()
     .domain([-10, 20])
     .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+
+	svg.append("g")
+		.attr("class", "axisDarkTheme")
+		.call(d3.axisLeft(y_axis));
+
 
   // Add dots
   svg.append('g')
@@ -36,9 +54,8 @@ d3.csv("https://raw.githubusercontent.com/ArielYssou/Site/master/uncommitted/ran
     .data(data)
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(d.x0); } )
-      .attr("cy", function (d) { return y(d.SalePrice); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
-
+      .attr("cx", function (d) { return x_axis(d.x0); } )
+      .attr("cy", function (d) { return y_axis(d.x1); } )
+      .attr("r", 3.5)
+      .style("fill", function(d) { return myColor(d.y) } )
 })
