f4207b7d67f59c4034be8ee7f24684a0d4365b36
ArielYssou <arielyssou@gmail.com>
2021-05-30 21:24:39 -0300

    major advances in d3 :D

--- uncommitted/randomForest/feature_space_split.js
+++ uncommitted/randomForest/feature_space_split.js
@@ -18,7 +18,27 @@ var myColor = d3.scaleOrdinal().domain(classes)
 
 var hyperplanes = new Array();
 hyperplanes.push(
-	{x1: , y1: , x2: , y2: ,depth: },
+	{value: -0.76, axis: 'x0', depth: 0},
+	{value: -8.00, axis: 'x0', depth: 1},
+	{value: -4.80, axis: 'x0', depth: 2},
+	{value: -3.21, axis: 'x0', depth: 4},
+	{value: -0.42, axis: 'x1', depth: 2},
+)
+
+var line = d3.line()
+    .x(function(d) { return x(d.dwte); })
+    .y(function(d) { return y(d.close); });
+
+var decision_lines = new Array();
+decision_lines.push(
+	{
+		id: 0,
+		values: [{'x': -15,'y': -0.42}, {'x': -3.21, 'y': -0.42}, {'x': -3.21, 'y': -10}],
+	},
+	{
+		id: 1,
+		values: [{'x': -4.80, 'y': 20}, {'x': -4.80, 'y': 1.12}, {'x': -0.76, 'y': 1.12}, {'x': -0.76, 'y': 20}],
+	},
 )
 
 //Read the data
@@ -27,15 +47,15 @@ d3.csv("https://raw.githubusercontent.com/ArielYssou/Site/master/uncommitted/ran
   // Add X axis
   var x_axis = d3.scaleLinear()
     .domain([-15, 10])
-    .range([ 0, width]);
+    .range([0, width]);
 
   // Add Y axis
   var y_axis = d3.scaleLinear()
     .domain([-10, 20])
-    .range([ height, 0]);
+    .range([height, 0]);
 
   // Add dots
-  svg.append('g')
+  dots = svg.append('g')
     .selectAll("dot")
     .data(data)
     .enter()
@@ -43,15 +63,67 @@ d3.csv("https://raw.githubusercontent.com/ArielYssou/Site/master/uncommitted/ran
       .attr("cx", function (d) { return x_axis(d.x0); } )
       .attr("cy", function (d) { return y_axis(d.x1); } )
       .attr("r", 3.5)
-      .style("fill", function(d) { return myColor(d.y) } )
+      .style("fill", "#444444")
 
-	svg.append('g')
-		.selectAll("line")
-		.data(hyperplanes)
-		.enter()
-		.append('line')
-			.attr('x1', function(d) { return x_axis(d.x1) } )
-			.attr('y1', function(d) { return y_axis(d.y1) } )
-			.attr('x2', function(d) { return x_axis(d.x2) } )
-			.attr('y2', function(d) { return y_axis(d.y2) } )
+	 svg.append("g")
+			.selectAll("paths")
+			.data(decision_lines)
+			.enter()
+			.append("path")
+				.attr("d", function (d) {
+						return d3.line()
+								.x(d => x_axis(d.x))
+								.y(d => y_axis(d.y))
+								(d.values)
+				})
+				.attr("fill", "none")
+				.attr("stroke", "#ffeabc")
+				.attr("stroke-width", 2)
+				.attr("id" , function(d){
+					return "line" + d.id;
+				})
+	console.log(path)
+	for(var idx = 0; idx < 2; idx += 1) {
+		var path = svg.select('#line' + idx)
+		console.log('#line' + idx)
+		console.log(path)
+		var totalLength = path.node().getTotalLength();
+
+		path
+			.attr("stroke-dasharray", totalLength + " " + totalLength)
+			.attr("stroke-dashoffset", totalLength)
+			.transition()
+				//.delay( function(i) { return i * 1000} )
+				.duration(1500)
+				.ease(d3.easeLinear)
+				.attr("stroke-dashoffset", 0)
+				.on("end", colorPoints);
+	}
+
+//	svg.append('g')
+//		.selectAll("line")
+//		.data(hyperplanes)
+//		.enter()
+//		.append('line')
+//			.attr('x1', function(d) { return (d.axis == 'x0' ? x_axis(d.value): 0) } )
+//			.attr('x2', function(d) { return (d.axis == 'x0' ? x_axis(d.value): 0) } )
+//			.attr('y1', function(d) { return (d.axis == 'x0' ? height: y_axis(d.value)) } )
+//			.attr('y2', function(d) { return (d.axis == 'x0' ? height: y_axis(d.value)) } )
+//      .attr("stroke", "#ffeabc")
+//      .attr("stroke-width", 1.5)
+//			.attr("opacity", 0.5)
+//		.transition()
+//			.duration(2000)
+//			.delay(function(d){ return ( d.depth * 500) })
+//			.attr('x2', function(d) { return (d.axis == 'x0' ? x_axis(d.value) : width) } )
+//			.attr('y2', function(d) { return (d.axis == 'x0' ? 0 : y_axis(d.value)) } )
+//
 })
+
+function colorPoints() {
+	console.log('hi')
+	d3.selectAll("circle").transition()
+			.delay(function(d, i){return(i * 5)})
+			.duration(100)
+			.style("fill", function(d) { return myColor(d.y) } )
+}  
