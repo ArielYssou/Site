ad1ee355891d9b9873d3fea275135a111e37c09f
ArielYssou <arielyssou@gmail.com>
2021-11-14 17:55:48 -0300

    finsihed basic theory

--- blog/physics/randomWalk/d3_charts.js
+++ blog/physics/randomWalk/d3_charts.js
@@ -6,7 +6,7 @@ function random_walker(run, p=0.5, steps=250) {
 	var positions = new Array();
 	var current_pos = 0;
 
-	for(var step = 0; step <= steps; step += 1){
+	for(var step = 0; step <= steps; step += 1) {
 		direction = (Math.random() < p) ? 1 : -1;
 		current_pos += direction;
 		positions.push({
@@ -15,6 +15,19 @@ function random_walker(run, p=0.5, steps=250) {
 			step: step,
 		})
 	}
+	return positions;
+}
+
+function rw_boundry(which='upper', p=0.5, steps=250, confidence_interval=3) {
+	var sign = which == 'upper' ? 1 : -1;
+	var positions = new Array();
+	for(var i = 0; i <= steps; i += 1) {
+		positions.push({
+			x: i,
+			y: sign * confidence_interval * Math.sqrt(4 * (p * (1 - p)) * i),
+		})
+	}
+
 	return positions;
 }
 
@@ -28,6 +41,8 @@ function RandomWalkChart() {
 			width = total_width - margin.left - margin.right,
 			height = total_height - margin.top - margin.bottom;
 
+	var _max_steps = 350; _y_lim = 70;
+
   function my(selection) {
 		// append the svg object to the body of the page
 		svg_random_walk = selection.append("svg")
@@ -39,12 +54,12 @@ function RandomWalkChart() {
 
 		// Add X axis
 		var x_axis_plot = d3.scaleLinear()
-			.domain([0, 250])
+			.domain([0, _max_steps])
 			.range([0, width]);
 
 		// Add Y axis
 		var y_axis_plot = d3.scaleLinear()
-			.domain([-70, 70])
+			.domain([-_y_lim, _y_lim])
 			.range([height, 0]);
 
 		// Axes
@@ -80,19 +95,46 @@ function RandomWalkChart() {
 		// Make a random walk
 		// ref: https://stackoverflow.com/questions/8689498/drawing-multiple-lines-in-d3-js
 		for(run = 0; run < 50; run += 1) {
-			positions = random_walker(run)
+			positions = random_walker(run, p=0.5, _max_steps)
 			rw_plot = svg_random_walk.append("path")
 					.datum(positions)
 					.attr("stroke", "#FF7E47")
 					.attr('fill', 'none')
 					.attr('opacity', 0.3)
-					.attr("stroke-width", 1.5)
+					.attr("stroke-width", 2)
 					.attr('d', d3.line()
 						.x(function(d) { return x_axis_plot(d.step); })
 						.y(function(d) { return y_axis_plot(d.position); })
 						// .curve(d3.curveCatmullRom.alpha(0.5))
 					)
 		}
+
+		var upper_boundry = rw_boundry('upper', p=0.5, _max_steps, confidence_interval=3);
+		console.log(upper_boundry)
+		svg_random_walk.append("path")
+				.datum(upper_boundry)
+				.attr("stroke", "#ffffff")
+				.attr('fill', 'none')
+				.attr("stroke-width", 2.0)
+				.attr("stroke-dasharray", (10, 5))
+				.attr('d', d3.line()
+					.x(function(d) { return x_axis_plot(d.x); })
+					.y(function(d) { return y_axis_plot(d.y); })
+					// .curve(d3.curveCatmullRom.alpha(0.5))
+				)
+
+		var lower_boundry = rw_boundry('lower', p=0.5, _max_steps)
+		svg_random_walk.append("path")
+				.datum(lower_boundry)
+				.attr("stroke", "#ffffff")
+				.attr('fill', 'none')
+				.attr("stroke-width", 2.0)
+				.attr("stroke-dasharray", (10, 5))
+				.attr('d', d3.line()
+					.x(function(d) { return x_axis_plot(d.x); })
+					.y(function(d) { return y_axis_plot(d.y); })
+					// .curve(d3.curveCatmullRom.alpha(0.5))
+				)
 
 		// Plot markers
 		/*
@@ -126,7 +168,6 @@ function RandomWalkChart() {
 
 var svg_entropy_plot = d3.select("#rw_plot")
 
-rw_plot_chart = RandomWalkChart().width(400).height(250)
+rw_plot_chart = RandomWalkChart().width(600).height(250)
 rw_plot_chart(svg_entropy_plot)
-rw_plot_chart.createVerticalLine(0)
 
